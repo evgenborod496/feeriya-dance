02531ae1ab29c6db1428001201f36f6ed03a3bbb
evgenborod496 <evgenborod496@gmail.com>
2019-02-15 21:56:59 +0300

    feat: added adaptability for all pages

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
 var menu = document.querySelector(".nav__menu");
 var nav = document.querySelector(".nav__link-section");
+var options = document.querySelectorAll(".options__item");
 menu.addEventListener('click', function () {
   if (nav.classList.contains("nav_hidden"))
     nav.classList.remove("nav_hidden");
   else
     nav.classList.add("nav_hidden");
 })
+
+document.addEventListener('click', function (e) {
+  if(e.target.classList.contains("categories__map")) {
+    document.querySelector(".iframe-map").classList.add("show");
+  }
+  else
+    document.querySelector(".iframe-map").classList.remove("show");
+})
+
+for (var j=0; j<options.length; j++){
+options[j].addEventListener('click', function (e) {
+  var z;
+  for (z=0; z<options.length; z++){
+    if(options[z]==e.currentTarget){
+      break;
+    }
+  }
+  var buf = options[z].querySelectorAll(".options__text");
+  if (options[z].querySelector(".options__text").classList.contains("options__text_hidden")) {
+    for (var i=0; i<buf.length; i++){
+          buf[i].classList.remove("options__text_hidden");
+    }
+  }else
+  {
+      for (var i=0; i<buf.length; i++){
+            buf[i].classList.add("options__text_hidden");
+  }
+  }
+})
+}
