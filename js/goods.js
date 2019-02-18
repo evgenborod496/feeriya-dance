562766be7f2bc791c65c6d35191eeffd158760c3
evgenborod496 <evgenborod496@gmail.com>
2019-02-18 21:40:59 +0300

    feat: added new pages to web-site

---
+++ js/goods.js
@@ -0,0 +1,159 @@
+// GOODS
+
+let allGoods = [{
+    nameCategory: 'shoes', //туфли
+    goods: [{
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'платье "рысь"'
+    }]
+},
+{
+    nameCategory: 'shirt',
+    goods: [{
+        photoSrc: 'img/test.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },]
+},
+{
+    nameCategory: 'bele',
+    goods: [{
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'белье "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },]
+},
+{
+    nameCategory: 'olimpiki',
+    goods: [{
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'олимпийка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },]
+},
+{
+    nameCategory: 'shorts',
+    goods: [{
+        photoSrc: 'img/test.png',
+        linkSrc: '#',
+        description: 'шорты "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/test2.jpg',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },]
+}];
+
+window.allGoods = allGoods;
--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 var menu = document.querySelector(".nav__menu");
 var nav = document.querySelector(".nav__link-section");
 var options = document.querySelectorAll(".options__item");
+
 menu.addEventListener('click', function () {
   if (nav.classList.contains("nav_hidden"))
     nav.classList.remove("nav_hidden");
   else
     nav.classList.add("nav_hidden");
-})
-
-document.addEventListener('click', function (e) {
-  if (e.target.classList.contains("categories__map")) {
-    document.querySelector(".iframe-map").classList.add("show");
-  }
-  else
-    document.querySelector(".iframe-map").classList.remove("show");
-})
+});
 
 for (var j = 0; j < options.length; j++) {
   options[j].querySelector(".options__title").addEventListener('click', function (e) {
@@ -36,3 +29,51 @@ for (var j = 0; j < options.length; j++) {
     }
   })
 }
+
+
+// CONSTRUCTOR ITEMS 
+
+;(() => {
+  let goodsIndent = document.querySelector('.goods__indent');
+  let optionsText = document.querySelectorAll('.options__text');
+  optionsText = Array.from(optionsText);
+
+  optionsText.forEach(item => {
+    item.addEventListener('click', () => {
+      let allItems = document.querySelectorAll('.goods__item:not(.goods__item_example)');
+      allItems = Array.from(allItems); 
+
+      for(let i = 0; i < allItems.length; i++){
+        goodsIndent.removeChild(allItems[i]);
+      }
+        
+      getGoodsForName(item.getAttribute('data-name-section'));
+    });
+  });
+
+
+  let goodsExample = document.querySelector('.goods__item_example');
+
+
+  function createItems(options){
+    let item = goodsExample.cloneNode(true);
+    item.classList.remove('goods__item_example');
+    item.querySelector('.goods__image-photo').setAttribute('src', options.photoSrc);
+    item.querySelector('.goods__image').setAttribute('src', options.linkSrc);
+    item.querySelector('.goods__description').innerHTML = options.description;
+
+    goodsIndent.appendChild(item);
+
+  }
+
+  function getGoodsForName(name){
+    window.allGoods.forEach(item => {
+      if(item.nameCategory === name){
+        for(let i = 0; i < item.goods.length; i++){
+          createItems(item.goods[i]);
+        }
+      }
+    });
+  }
+  window.constructorGoods = getGoodsForName;
+})();  
