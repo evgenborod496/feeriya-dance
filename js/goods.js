d38ca9fb67f80bb7f4b6024baa32b97f80bae165
evgenborod496 <evgenborod496@gmail.com>
2019-03-16 23:17:41 +0300

    fix: - add some test photos

--- js/goods.js
+++ js/goods.js
@@ -49,22 +49,92 @@ let allGoods = [{
     }]
 },
 {
-    nameCategory: 'shirt',
+    nameCategory: 'swimwear',
     goods: [{
-        photoSrc: 'img/test.png',
+        photoSrc: 'img/gymnastics/underwear/4-removebg.png',
+        linkSrc: '#',
+        description: 'купальник "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/5-removebg.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
     },
     {
-        photoSrc: 'img/test.png',
+        photoSrc: 'img/gymnastics/underwear/6-removebg.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
     },
     {
-        photoSrc: 'img/test.png',
+        photoSrc: 'img/gymnastics/underwear/7.PNG',
         linkSrc: '#',
         description: 'рубашка "рысь"'
-    },]
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/18-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/b_21_011_black_bk9q1400-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g_21_011_beluii_3768-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g_21_011_spinka_3751-500x750-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g_33_301_1127-500x750-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g_33_301_bel_1474-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g_41_011_beluii_3785-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g_41_011_spinka_3917-500x750-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g13_301_rozoviy_BK9Q5390-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/g23_301_rozoviy_BK9Q5113-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/PK_T4-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/PK_T-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/BK9Q1549_1-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
 },
 {
     nameCategory: 'bele',
