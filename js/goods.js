c4c59091743a2e91db06b368edb052e2d2c32759
evgenborod496 <evgenborod496@gmail.com>
2019-03-17 18:54:49 +0300

    feat: - add a lot of photos of goods

--- js/goods.js
+++ js/goods.js
@@ -1,52 +1,12 @@
 // GOODS
 
 let allGoods = [{
-    nameCategory: 'shoes', //туфли
+    nameCategory: 'b-shoes', //туфли
     goods: [{
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/dance/for-boys/shoes/b-shoes-black.png',
         linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'платье "рысь"'
-    }]
+        description: 'туфли "рысь"'
+    },]
 },
 {
     nameCategory: 'swimwear',
@@ -123,7 +83,7 @@ let allGoods = [{
     {
         photoSrc: 'img/gymnastics/underwear/PK_T4-200x300-removebg.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Топ-невидимка (хлопок)'
     },
     {
         photoSrc: 'img/gymnastics/underwear/PK_T-200x300-removebg.png',
@@ -134,96 +94,534 @@ let allGoods = [{
         photoSrc: 'img/gymnastics/underwear/BK9Q1549_1-200x300-removebg.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/underwear/sdt43rtegfb43er.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
     }]
 },
 {
-    nameCategory: 'bele',
+    nameCategory: 'underpants',
     goods: [{
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/underpants/sdg44e5terg43e.png',
         linkSrc: '#',
-        description: 'белье "рысь"'
+        description: 'Трусы "рысь"'
+    }]
+},
+{
+    nameCategory: 'b-shirts',
+    goods: [{
+        photoSrc: 'img/dance/for-boys/shirts/r_55_331_bel_bk9q0616-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'Рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'maces',
+    goods: [{
+        photoSrc: 'img/gymnastics/accessories/maces/sbg-500x750.png',
+        linkSrc: '#',
+        description: 'Булава "рысь"'
+    }]
+},
+{
+    nameCategory: 'balls',
+    goods: [{
+        photoSrc: 'img/gymnastics/accessories/balls/sadf433red.png',
+        linkSrc: '#',
+        description: 'Булава "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/balls/smg_-500x750.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Мяч "рысь"'
+    }]
+},
+{
+    nameCategory: 'knee-pads',
+    goods: [{
+        photoSrc: 'img/gymnastics/accessories/knee-pads/gh45ryh45rtdf.png',
+        linkSrc: '#',
+        description: 'Наколенники "рысь"'
+    }]
+},
+{
+    nameCategory: 'jump-ropes',
+    goods: [{
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/07926_fiol-500x750.png',
+        linkSrc: '#',
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/07927_red-500x750.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/07928_cyan-500x750.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/07931_pink-500x750 (1).png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/07932_green-500x750.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/08196_salat-500x750.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/sdgb4tyh54rt.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/jump-ropes/trffdtg45rt.png',
         linkSrc: '#',
-        description: 'рубашка "рысь"'
-    },
-    {
-        photoSrc: 'img/test2.jpg',
-        linkSrc: '#',
-        description: 'рубашка "рысь"'
+        description: 'Скакалка "рысь"'
     },]
 },
 {
-    nameCategory: 'olimpiki',
+    nameCategory: 'cases',
     goods: [{
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/cases/K5847_0009-200x300.png',
         linkSrc: '#',
-        description: 'олимпийка "рысь"'
+        description: 'чехол "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/cases/K5848_0008-200x300.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/cases/K5849_0006-200x300.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
-    },]
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/cases/K5855_0015-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/cases/K5856_0020-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
 },
 {
-    nameCategory: 'shorts',
+    nameCategory: 'expander-tape',
     goods: [{
-        photoSrc: 'img/test.png',
+        photoSrc: 'img/gymnastics/accessories/expander-tape/07685035-200x300.png',
         linkSrc: '#',
-        description: 'шорты "рысь"'
+        description: 'Эспандер-лента "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/expander-tape/b0d55a1a1f7eb4e0acb1b763d3992feb.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
     },
     {
-        photoSrc: 'img/test2.jpg',
+        photoSrc: 'img/gymnastics/accessories/expander-tape/11368_ (2)-200x300.png',
         linkSrc: '#',
         description: 'рубашка "рысь"'
-    },]
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/expander-tape/11368_ (1)-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/expander-tape/11367_-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/expander-tape/11366_-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'expander-loop',
+    goods: [{
+        photoSrc: 'img/gymnastics/accessories/expander-loop/11368_ (3)-200x300.png',
+        linkSrc: '#',
+        description: 'Бабочка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/expander-loop/11368_ (4)-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/accessories/expander-loop/11368_ (5)-200x300.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'ballet-shoes',
+    goods: [{
+        photoSrc: 'img/gymnastics/shoes/ballet-shoes/fdg34ertf.png',
+        linkSrc: '#',
+        description: 'Баллетки "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/shoes/ballet-shoes/sdf345ter23wesd.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/shoes/ballet-shoes/sadfs32s.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'jazz',
+    goods: [{
+        photoSrc: 'img/gymnastics/shoes/jazz/drf4ertdg34er.png',
+        linkSrc: '#',
+        description: 'Джазовки "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/shoes/jazz/dft43ergr43er.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'b-ties',
+    goods: [{
+        photoSrc: 'img/dance/for-boys/ties/6a545e45080c1000245570e6670c66b6.png',
+        linkSrc: '#',
+        description: 'Бабочка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-boys/ties/sds9f0sdf09s87f0sd.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-boys/ties/sdofus9d7g98safu8sad7gs98.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'foot-protection',
+    goods: [{
+        photoSrc: 'img/gymnastics/shoes/foot-protection/IMG_20190126_204137-removebg.png',
+        linkSrc: '#',
+        description: 'Защита для стопы "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/shoes/foot-protection/IMG_20190126_204216-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/gymnastics/shoes/foot-protection/IMG_20190126_204246-removebg.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    }]
+},
+{
+    nameCategory: 'b-hats',
+    goods: [{
+        photoSrc: 'img/swimming/for-boys/hats/06322-200x300.png',
+        linkSrc: '#',
+        description: 'Шапочка "рысь"'
+    }]
+},
+{
+    nameCategory: 'b-glasses',
+    goods: [{
+        photoSrc: 'img/swimming/for-boys/glasses/06131-200x300.png',
+        linkSrc: '#',
+        description: 'Шапочка "рысь"'
+    }]
+},
+{
+    nameCategory: 'swimming-trunks',
+    goods: [{
+        photoSrc: 'img/swimming/for-boys/swimming-trunks/p_57_012_cvet16_3474-200x300.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    }]
+},
+{
+    nameCategory: 'g-hats',
+    goods: [{
+        photoSrc: 'img/swimming/for-girls/hats/06322-200x300.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    }]
+},
+{
+    nameCategory: 'g-swimmwear',
+    goods: [{
+        photoSrc: 'img/swimming/for-girls/swimwear/k_40_012_BK9Q3269_cvet_1-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_40_012_spinka_cvet1-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_45_014_1825_BK9Q0568-200x300.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_45_014_1827_BK9Q0490-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_45_014_back_BK9Q0494-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_47_012_2_cvet2_3456-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_47_012_cvet2-200x300.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_47_012_cvet3_BK9Q3408-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_48_011_cvet_3260-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k_48_011_fiolet50008_BK9Q3419-200x300.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    ,
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k40_014_1828_BK9Q0634-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    },
+    ,
+    {
+        photoSrc: 'img/swimming/for-girls/swimwear/k40_014_1831_BK9Q0556-500x750.png',
+        linkSrc: '#',
+        description: 'Плавки "рысь"'
+    }]
+},
+{
+    nameCategory: 'b-trousers',
+    goods: [{
+        photoSrc: 'img/dance/for-boys/trousers/r_55_331_bel_bk9q0616-200x300.png',
+        linkSrc: '#',
+        description: 'Брюки "рысь"'
+    }]
+},
+{
+    nameCategory: 'g-glasses',
+    goods: [{
+        photoSrc: 'img/swimming/for-girls/glasses/06131-200x300.png',
+        linkSrc: '#',
+        description: 'Очки "рысь"'
+    }]
+},
+{
+    nameCategory: 'b-trousers',
+    goods: [{
+        photoSrc: 'img/dance/for-boys/trousers/r_55_331_bel_bk9q0616-200x300.png',
+        linkSrc: '#',
+        description: 'Брюки "рысь"'
+    }]
+},
+{
+    nameCategory: 'shoes-accessories',
+    goods: [{
+        photoSrc: 'img/dance/accessories/for-shoes/sdffd7dg97d9fg7d9f.png',
+        linkSrc: '#',
+        description: 'Накаблучники "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-shoes/dft4534egd5463edh4.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-shoes/bags___b71cdb49-165x200xcxb-ci.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-shoes/sdf8s68s6d8sd8f6sdg.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    }]
+},
+{
+    nameCategory: 'hair-accessories',
+    goods: [{
+        photoSrc: 'img/dance/accessories/for-hair/dg45jgfy576f43h.png',
+        linkSrc: '#',
+        description: 'Накаблучники "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-hair/dff657767jmg756f.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-hair/sgd46ghfh56rfjf.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-hair/df634gf656r56dgdf.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-hair/gdf54hjkjhf6564gfn.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-hair/f546fcb55r6edgb.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/for-hair/fdh556ejg654.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    }]
+},
+{
+    nameCategory: 'g-tights',
+    goods: [{
+        photoSrc: 'img/dance/for-girls/tights/f565edfhr56df.png',
+        linkSrc: '#',
+        description: 'Колготки "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/df53ed5edg5td.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/asfsdf43rfg6ed.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/steggr4e534fdgr.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/gtrfhc654hfy4.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/df54tgfc54rf.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/sdf345fg34e534erg.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/for-girls/tights/fdt435dfb4rt5dg.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    }]
+},
+{
+    nameCategory: 'cosmetics',
+    goods: [{
+        photoSrc: 'img/dance/accessories/cosmetics/sdf345dfg43er.png',
+        linkSrc: '#',
+        description: 'Колготки "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/cosmetics/dsg5534eg4erdfx.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/cosmetics/drg45etdg4er.png',
+        linkSrc: '#',
+        description: 'рубашка "рысь"'
+    },
+    {
+        photoSrc: 'img/dance/accessories/cosmetics/sdg56r6yhn76s.png',
+        linkSrc: '#',
+        description: 'Щетка "рысь"'
+    }]
+},
+{
+    nameCategory: 'false-eyelashes',
+    goods: [{
+        photoSrc: 'img/dance/accessories/false-eyelashes/sdfgb54eryhh54re.png',
+        linkSrc: '#',
+        description: 'Ресницы "рысь"'
+    }]
+},
+{
+    nameCategory: 'g-dresses',
+    goods: [{
+        photoSrc: 'img/dance/for-girls/dresses/r_33_051_morsk_7159__3516_-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'Платье "рысь"'
+    },{
+        photoSrc: 'img/dance/for-girls/dresses/r_35_051_malin_3181__3829_-200x300-removebg.png',
+        linkSrc: '#',
+        description: 'Платье "рысь"'
+    }]
+},
+{
+    nameCategory: 'g-shoes',
+    goods: [{
+        photoSrc: 'img/dance/for-girls/shoes/sdfxg54trth4d.png',
+        linkSrc: '#',
+        description: 'Платье "рысь"'
+    },{
+        photoSrc: 'img/dance/for-girls/shoes/sdfg454tyh45r.png',
+        linkSrc: '#',
+        description: 'Платье "рысь"'
+    },{
+        photoSrc: 'img/dance/for-girls/shoes/dsg45534trgh435r.png',
+        linkSrc: '#',
+        description: 'Платье "рысь"'
+    },{
+        photoSrc: 'img/dance/for-girls/shoes/th5rtdfghn45645rt.png',
+        linkSrc: '#',
+        description: 'Платье "рысь"'
+    }]
 }];
 
 window.allGoods = allGoods;
